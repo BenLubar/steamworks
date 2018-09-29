a7a84984f521d364fe6a86a188f264a8f6288210
Ben Lubar <ben.lubar@gmail.com>
2018-09-29 15:29:13 -0500

    Improve formatting of InitClient documentation on godoc.org.

--- callback.go
+++ callback.go
@@ -86,20 +86,22 @@ var (
 // Returns nil if all required interfaces have been acquired and are accessible.
 //
 // A non-nil error indicates one of the following conditions:
-// - The Steam client isn't running. A running Steam client is required to
-//   provide implementations of the various Steamworks interfaces.
-// - The Steam client couldn't determine the App ID of game. If you're running
-//   your application from the executable or debugger directly then you must
-//   have a steam_appid.txt in your game directory next to the executable, with
-//   your app ID in it and nothing else. Steam will look for this file in the
-//   current working directory. If you are running your executable from a
-//   different directory you may need to relocate the steam_appid.txt file.
-// - Your application is not running under the same OS user context as the
-//   Steam client, such as a different user or administration access level.
-// - Ensure that you own a license for the App ID on the currently active Steam
-//   account. Your game must show up in your Steam library.
-// - Your App ID is not completely set up, i.e. in Release State: Unavailable,
-//   or it's missing default packages.
+//
+//    - The Steam client isn't running. A running Steam client is required to
+//      provide implementations of the various Steamworks interfaces.
+//    - The Steam client couldn't determine the App ID of game. If you're
+//      running your application from the executable or debugger directly then
+//      you must have a steam_appid.txt in your game directory next to the
+//      executable, with your app ID in it and nothing else. Steam will look
+//      for this file in the current working directory. If you are running your
+//      executable from a different directory you may need to relocate the
+//      steam_appid.txt file.
+//    - Your application is not running under the same OS user context as the
+//      Steam client, such as a different user or administration access level.
+//    - Ensure that you own a license for the App ID on the currently active
+//      Steam account. Your game must show up in your Steam library.
+//    - Your App ID is not completely set up, i.e. in Release State:
+//      Unavailable, or it's missing default packages.
 func InitClient(startCallbackGoroutine bool) error {
 	if !internal.SteamAPI_Init() {
 		if !internal.SteamAPI_IsSteamRunning() {
