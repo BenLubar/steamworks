bc868af77643e4b2be8d7ef8a51d6c5e0ba0981a
Ben Lubar <ben.lubar@gmail.com>
2018-09-29 14:15:21 -0500

    Explicitly mark Reliability constants' type to improve documentation formatting.

--- steamnet/send.go
+++ steamnet/send.go
@@ -13,26 +13,36 @@ import (
 type Reliability = internal.EP2PSend
 
 const (
-	// Unreliable is a basic UDP send. Packets can't be bigger than 1200 bytes (your typical MTU size). Can be lost, or arrive out of order (rare).
-	// The sending API does have some knowledge of the underlying connection, so if there is no NAT-traversal accomplished or
-	// there is a recognized adjustment happening on the connection, the packet will be batched until the connection is open again.
-	Unreliable = internal.EP2PSend_Unreliable
+	// Unreliable is a basic UDP send. Packets can't be bigger than 1200 bytes
+	// (your typical MTU size). Can be lost, or arrive out of order (rare). The
+	// sending API does have some knowledge of the underlying connection, so if
+	// there is no NAT-traversal accomplished or there is a recognized
+	// adjustment happening on the connection, the packet will be batched until
+	// the connection is open again.
+	Unreliable Reliability = internal.EP2PSend_Unreliable
 
-	// UnreliableNoDelay works like Unreliable, but if the underlying P2P connection isn't yet established the packet will just be thrown away.
-	// Using this on the first packet sent to a remote host almost guarantees the packet will be dropped.
-	// This is only really useful for kinds of data that should never buffer up, e.g. voice payload packets
-	UnreliableNoDelay = internal.EP2PSend_UnreliableNoDelay
+	// UnreliableNoDelay works like Unreliable, but if the underlying P2P
+	// connection isn't yet established the packet will just be thrown away.
+	// Using this on the first packet sent to a remote host almost guarantees
+	// the packet will be dropped. This is only really useful for kinds of data
+	// that should never buffer up, e.g. voice payload packets
+	UnreliableNoDelay Reliability = internal.EP2PSend_UnreliableNoDelay
 
 	// Reliable message send. Can send up to 1MB of data in a single message.
-	// Does fragmentation/re-assembly of messages under the hood, as well as a sliding window for efficient sends of large chunks of data.
-	Reliable = internal.EP2PSend_Reliable
+	// Does fragmentation/re-assembly of messages under the hood, as well as a
+	// sliding window for efficient sends of large chunks of data.
+	Reliable Reliability = internal.EP2PSend_Reliable
 
-	// ReliableWithBuffering works like Reliable, but applies the Nagle algorithm to the send - sends will accumulate until the current MTU size (typically ~1200 bytes, but can change)
-	// or ~200ms has passed (Nagle algorithm). This is useful if you want to send a set of smaller messages but have the coalesced into a single packet.
+	// ReliableWithBuffering works like Reliable, but applies the Nagle
+	// algorithm to the send - sends will accumulate until the current MTU size
+	// (typically ~1200 bytes, but can change) or ~200ms has passed (Nagle
+	// algorithm). This is useful if you want to send a set of smaller messages
+	// but have the coalesced into a single packet.
 	//
-	// Since the reliable stream is all ordered, you can do several small message sends with ReliableWithBuffering and then do a normal Reliable
+	// Since the reliable stream is all ordered, you can do several small
+	// message sends with ReliableWithBuffering and then do a normal Reliable
 	// to force all the buffered data to be sent.
-	ReliableWithBuffering = internal.EP2PSend_ReliableWithBuffering
+	ReliableWithBuffering Reliability = internal.EP2PSend_ReliableWithBuffering
 )
 
 // Possible errors that can be returned by SendPacket.
